f973c153bef8c1a87c04b2a3b836ca9235135fe0
cheeseblurger <theslowlyslowly@gmail.com>
2013-11-29 19:37:16 -0500

    updated CV

--- Melinda_Richter/Home_files/Home.js
+++ Melinda_Richter/Home_files/Home.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20130718
+// Created by iWeb 3.0.4 local-build-20131129
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
