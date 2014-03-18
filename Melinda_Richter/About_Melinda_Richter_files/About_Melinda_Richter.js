6132c63e73b2f3e7944c1589c7f4a42c81a966c2
cheeseblurger <theslowlyslowly@gmail.com>
2014-03-18 16:00:48 -0400

    hopefully I fixed it this time

---
+++ Melinda_Richter/About_Melinda_Richter_files/About_Melinda_Richter.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20140318
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('About_Melinda_Richter_files/About_Melinda_RichterMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
