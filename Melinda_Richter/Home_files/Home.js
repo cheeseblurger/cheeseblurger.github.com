42047d71728bb33b854ebb7213e7e52b605ea058
cheeseblurger <theslowlyslowly@gmail.com>
2014-07-13 11:39:44 -0400

    adjusted home page

--- Melinda_Richter/Home_files/Home.js
+++ Melinda_Richter/Home_files/Home.js
@@ -1,11 +1,11 @@
 // Created by iWeb 3.0.4 local-build-20140713
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_3:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_1:new IWShadow({blurRadius:3,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.750000}),shadow_2:new IWShadow({blurRadius:3,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:3,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000}),shadow_3:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_1:new IWShadow({blurRadius:3,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.750000}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Home_files/HomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('Home_files','shapeimage_3','0');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('Home_files','shapeimage_2','0');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
--- Melinda_Richter/PreviousProjects_files/PreviousProjects.js
+++ Melinda_Richter/PreviousProjects_files/PreviousProjects.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20140709
+// Created by iWeb 3.0.4 local-build-20140713
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000})});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('PreviousProjects_files/PreviousProjectsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('PreviousProjects_files','shapeimage_6','0');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');IMpreload('PreviousProjects_files','shapeimage_6','0');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
