d1465ad321f0eee0cc576b989f3d546d3e3fac87
cheeseblurger <theslowlyslowly@gmail.com>
2015-09-02 15:48:39 -0400

    updated cv

--- Melinda_Richter/About_Melinda_Richter_files/About_Melinda_Richter.js
+++ Melinda_Richter/About_Melinda_Richter_files/About_Melinda_Richter.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20140713
+// Created by iWeb 3.0.4 local-build-20150902
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000})});registry.applyEffects();}
--- Melinda_Richter/Home_files/Home.js
+++ Melinda_Richter/Home_files/Home.js
@@ -1,7 +1,7 @@
-// Created by iWeb 3.0.4 local-build-20140713
+// Created by iWeb 3.0.4 local-build-20150902
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:3,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000}),shadow_3:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_1:new IWShadow({blurRadius:3,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.750000}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),shadow_0:new IWShadow({blurRadius:3,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.750000}),shadow_2:new IWShadow({blurRadius:3,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000}),shadow_3:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),shadow_1:new IWShadow({blurRadius:3,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.750000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
